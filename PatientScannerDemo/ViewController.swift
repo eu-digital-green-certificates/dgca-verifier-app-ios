09684f21c726edca87500818e1374bc0f6881773
Yannick Spreen <yannick.spreen@gmail.com>
2021-04-08 21:12:43 +0200

    Initial Commit

---
+++ PatientScannerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatientScannerDemo
+//
+//  Created by Yannick Spreen on 4/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
