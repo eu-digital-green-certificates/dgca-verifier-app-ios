343dc7380078dc264d10f6dea70c39496948c6e7
Yannick Spreen <yannick.spreen@gmail.com>
2021-04-17 01:23:25 +0200

    Streamline signature service.

--- PatientScannerDemo/Signature.swift
+++ PatientScannerDemo/Signature.swift
@@ -11,43 +11,24 @@ import Foundation
 
 struct Signature {
   public static func verify(_ signature: Data, for data: Data, with publicKey: SecKey) -> Bool {
+    var signature = signature
+    var alg: SecKeyAlgorithm
+
     if SecKeyIsAlgorithmSupported(publicKey, .verify, .ecdsaSignatureMessageX962SHA256) {
-      return verifyEC(signature, for: data, with: publicKey)
+      alg = .ecdsaSignatureMessageX962SHA256
+      signature = ASN1.signature(from: signature)
+    } else if SecKeyIsAlgorithmSupported(publicKey, .verify, .rsaSignatureMessagePSSSHA256) {
+      alg = .rsaSignatureMessagePSSSHA256
+    } else {
+      return false
     }
-    if SecKeyIsAlgorithmSupported(publicKey, .verify, .rsaSignatureMessagePSSSHA256) {
-      return verifyRSA(signature, for: data, with: publicKey)
-    }
-    return false
-  }
-
-  static func verifyEC(_ signature: Data, for data: Data, with publicKey: SecKey) -> Bool {
-    let sig = ASN1.signature(from: signature)
 
     var error: Unmanaged<CFError>?
     let result = SecKeyVerifySignature(
       publicKey,
-      .ecdsaSignatureMessageX962SHA256,
+      alg,
       data as NSData,
-      sig as NSData,
-      &error
-    )
-    if let err = error?.takeUnretainedValue().localizedDescription {
-      print(err)
-    }
-    error?.release()
-
-    return result
-  }
-
-  static func verifyRSA(_ signature: Data, for data: Data, with publicKey: SecKey) -> Bool {
-    let sig = signature
-
-    var error: Unmanaged<CFError>?
-    let result = SecKeyVerifySignature(
-      publicKey,
-      .rsaSignatureMessagePSSSHA256,
-      data as NSData,
-      sig as NSData,
+      signature as NSData,
       &error
     )
     if let err = error?.takeUnretainedValue().localizedDescription {
