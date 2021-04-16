6b69ae3940f010b33aa3e2cc65442c290ec4e4e6
Yannick Spreen <yannick.spreen@gmail.com>
2021-04-16 17:00:00 +0200

    Simplify zlib.

--- PatientScannerDemo/ZLib.swift
+++ PatientScannerDemo/ZLib.swift
@@ -4,15 +4,7 @@ import Foundation
 import Compression
 
 func decompressString(_ data: Data) -> String {
-  let size = 4 * data.count
-  let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
-  let result = data.subdata(in: 2 ..< data.count).withUnsafeBytes ({
-    let read = compression_decode_buffer(buffer, size, $0.baseAddress!.bindMemory(to: UInt8.self, capacity: 1),
-                                         data.count - 2, nil, COMPRESSION_ZLIB)
-    return String(decoding: Data(bytes: buffer, count:read), as: UTF8.self)
-  }) as String
-  buffer.deallocate()
-  return result
+  return String(decoding: decompress(data), as: UTF8.self)
 }
 
 func decompress(_ data: Data) -> Data {
