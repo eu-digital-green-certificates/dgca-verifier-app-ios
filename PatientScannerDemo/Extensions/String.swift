7cc97a4b0d92f4db3c567243ced4354021e60813
Yannick Spreen <yannick.spreen@gmail.com>
2021-04-22 22:49:32 +0200

    Add project name to header.

--- PatientScannerDemo/Extensions/String.swift
+++ PatientScannerDemo/Extensions/String.swift
@@ -1,6 +1,6 @@
 /*-
  * ---license-start
- * eu-digital-green-certificates / dgca-verifier-app-web
+ * eu-digital-green-certificates / dgca-verifier-app-ios
  * ---
  * Copyright (C) 2021 T-Systems International GmbH and all other contributors
  * ---
