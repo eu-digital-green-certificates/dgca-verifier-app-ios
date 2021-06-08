4e394aac9faef75343acbe5a562d89da076c6ece
Light2288 <davide.aliti@gmail.com>
2021-06-08 15:33:23 +0200

    feat: add tests
    added HomeViewModelTests (WIP)

--- VerifierTests/HomeViewModelTests.swift
+++ VerifierTests/HomeViewModelTests.swift
@@ -1,26 +1,39 @@
 //
-//  DGCAVerifierTests.swift
-//  DGCAVerifierTests
+//  HomeViewModelTests.swift
+//  VerifierTests
 //
-//  Created by Yannick Spreen on 4/8/21.
+//  Created by Davide Aliti on 07/06/21.
 //
 
 import XCTest
-@testable import Verifier
+@testable import VerificaC19
 
-class VerifierTests: XCTestCase {
+class HomeViewModelTests: XCTestCase {
+    
+    var homeViewModel: HomeViewModel!
+    var localData: LocalData!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        homeViewModel = HomeViewModel()
+        localData = LocalData(encodedPublicKeys: [:], resumeToken: nil, lastFetchRaw: nil, settings: [])
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        homeViewModel = nil
     }
 
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testVersionOutdated() {
+        let setting = Setting(name: "ios", type: "APP_MIN_VERSION", value: "0.0.0")
+        LocalData.sharedInstance.settings.append(setting)
+        
+//        XCTAssertFalse(homeViewModel.isCurrentVersionOutdated())
     }
 
     func testPerformanceExample() throws {
