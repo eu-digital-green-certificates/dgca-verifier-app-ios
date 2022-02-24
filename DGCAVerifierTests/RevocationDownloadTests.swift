76deb56a690d21d8fa5efbc9f8c95288e7b28cc6
Denis Melenevsky <denys.melenevskyi@globallogic.com>
2022-02-24 12:13:54 +0200

    Test target (with not broken build) for revocation

---
+++ DGCAVerifierTests/RevocationDownloadTests.swift
@@ -0,0 +1,49 @@
+//
+/*-
+ * ---license-start
+ * eu-digital-green-certificates / dgca-verifier-app-ios
+ * ---
+ * Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+//  
+//  RevocationDownloadTests.swift
+//  DGCAVerifierTests
+//  
+//  Created by Denis Melenevsky on 22.02.2022.
+//  
+        
+
+import XCTest
+import DGCAVerifier
+//@testable import DGCAVerifier
+import SwiftDGC
+
+class RevocationDownloadTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let revocationService = RevocationService(baseServicePath: SharedConstants.revocationServiceBase)
+        
+        
+    }
+}
