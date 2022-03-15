4771ecf9aa8f71e898b5c0cebc89814b6fef122a
IgorKhomiak <ihorkhomyak@gmail.com>
2022-03-15 13:44:07 +0200

    Updated hash filter data

--- DGCAVerifier/Models/PartialHash/VariableHashFilter.swift
+++ DGCAVerifier/Models/PartialHash/VariableHashFilter.swift
@@ -36,6 +36,11 @@ public enum HashFilterError: Error {
 public class VariableHashFilter {
 
     private var size: UInt8
+    private var version: UInt16 = 1
+    private var probRate: Float = 0.0
+    private var currentElementAmount: Int = 0
+    private var definedElementAmount: Int = 0
+
     public private(set) var array: [BInt] = []
 
      /**
@@ -50,32 +55,44 @@ public class VariableHashFilter {
 
     public init?(data: Data) {
         guard !data.isEmpty else { return nil}
+                
+        self.version = data[0..<2].reversed().withUnsafeBytes {$0.load(as: UInt16.self)}
         
-        let byteArray = [UInt8](data)
-        self.size = byteArray[0]
-        let numHashes: Int = (byteArray.count - 1) / Int(size)
+        let bytes = Bytes(data[2..<5])
+        let bigEndianValue = bytes.withUnsafeBufferPointer {
+            $0.baseAddress!.withMemoryRebound(to: UInt32.self, capacity: 1) { $0.pointee }
+        }
+        let bitPattern = UInt32(bigEndian: bigEndianValue)
+        self.probRate = Float(bitPattern: bitPattern)
+        
+        self.definedElementAmount = data[5..<9].reversed().withUnsafeBytes {$0.load(as: Int.self)}
+        self.currentElementAmount = 0
+        
+        self.size = data[9..<10].withUnsafeBytes {$0.load(as: UInt8.self)}
+        let numHashes: Int = (data.count - 11) / Int(size)
         var bigArray: Array<BInt> = Array(repeating: BInt(signed: Bytes()), count: numHashes)
         
-        var hashNumCounter = 0
-        var counter = 1
-        while (counter < byteArray.count) {
-            let sliceArray = Array(byteArray[counter..<counter + Int(size)])
-            bigArray[hashNumCounter] = BInt(signed: sliceArray)
+        currentElementAmount = 0
+        var counter = 11
+        while (counter < data.count) {
+            let slice: [UInt8] = [UInt8](data[counter..<counter + Int(size)]).reversed()
+            bigArray[currentElementAmount] = BInt(signed: slice)
             counter += Int(size)
-            hashNumCounter += 1
+            currentElementAmount += 1
         }
-        self.array = bigArray
+        self.array = bigArray.sorted()
     }
+    
     /**
      * Check whether filter contains dcc hash bytes. It will check bytes depending on the filter size value.
      *
      * @param dccHashBytes byte array of dcc hash.
      * @return true is contains otherwise false
      */
-    public func mightContain(element: Data) -> Bool {
-        guard element.count >= size else { return false }
+    public func mightContain(element data: Data) -> Bool {
+        guard data.count >= size else { return false }
         
-        let dccHashBytes: [UInt8] = [UInt8](element)
+        let dccHashBytes: [UInt8] = [UInt8](data)
         let filterSizeBytes: [UInt8] = Array(dccHashBytes[0..<Int(size)])
         let rezult = binarySearch(bigInts: array, from: 0, to: array.count, element: BInt(signed: filterSizeBytes))
         return rezult
