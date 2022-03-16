f508fa31749ee3a950d2392572db1ba7bda60ab8
Denis Melenevsky <denys.melenevskyi@globallogic.com>
2022-03-16 02:42:03 +0200

    Revocations update test

--- DGCAVerifierDownloadTests/RevocationDownloadTests.swift
+++ DGCAVerifierDownloadTests/RevocationDownloadTests.swift
@@ -34,16 +34,51 @@ let mockRevocationListsResponse = """
     [{\"kid\":\"9cWXDDA52FQ=\",\"mode\":\"POINT\",\"hashTypes\":[\"SIGNATURE\"],\"expires\":\"2023-03-01T11:00:00Z\",\"lastUpdated\":\"2022-03-04T16:08:49Z\"},{\"kid\":\"GSXuNoyWGYo=\",\"mode\":\"POINT\",\"hashTypes\":[\"UCI\"],\"expires\":\"2023-11-27T10:15:14Z\",\"lastUpdated\":\"2022-03-09T11:29:40Z\"}]
     """
 
+let mockRevocationListsUpdateResponse = """
+    [{\"kid\":\"9cWXDDA52FQ=\",\"mode\":\"POINT\",\"hashTypes\":[\"SIGNATURE\"],\"expires\":\"2023-03-01T11:00:00Z\",\"lastUpdated\":\"2022-03-10T16:08:49Z\"},{\"kid\":\"GSXuNoyWGYo=\",\"mode\":\"POINT\",\"hashTypes\":[\"UCI\"],\"expires\":\"2023-11-27T10:15:14Z\",\"lastUpdated\":\"2022-03-11T11:29:40Z\"}]
+    """
+
+let mockRevocationListsDeleteResponse = """
+    [{\"kid\":\"9cWXDDA52FQ=\",\"mode\":\"POINT\",\"hashTypes\":[\"SIGNATURE\"],\"expires\":\"2023-03-01T11:00:00Z\",\"lastUpdated\":\"2022-03-10T16:08:49Z\"},{\"kid\":\"GSXuNoyWGYo=\",\"mode\":\"POINT\",\"hashTypes\":[\"UCI\"],\"expires\":\"2023-11-27T10:15:14Z\",\"lastUpdated\":\"2022-03-11T11:29:40Z\"}]
+    """
+
 let mockRevocationPartitionsResponse = """
+    [{\"kid\":\"GSXuNoyWGYo=\",\"id\":null,\"x\":null,\"y\":null,\"lastUpdated\":\"2022-03-09T11:29:40.029718Z\",\"expired\":\"2023-11-27T10:15:00Z\",\"chunks\":{\"2\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"0132815288af51fcfc709f422aee353687654513e4b105fdc8f6166e2df90bbb\"}},\"3\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"24352fcfac12fa8077f074fe6ba1814d09383db29899a1120ca6e587a0d7399d\"}},\"5\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"9e7556604c1774b2e5db4721a7a8ca4fe8c1aade97ad6386a0144218cc3bba56\"}},\"6\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"482dd47c5f344538302328c57e7dd211b27274a264d830f98a0d02d5bb163fc3\"}},\"f\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"75572ebd77dc4b07f49479b69aa7400c4a73472fd7b516dc1a10aa51dad86ea3\"}},\"8\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"1e84743ec149ed4c5f02abe27dab0ef0bade3e8882a8d2907419b252c7a768b0\"}}}}]
+    """
+
+let mockRevocationPartitionsUpdateResponse = """
+    [{\"kid\":\"GSXuNoyWGYo=\",\"id\":null,\"x\":null,\"y\":null,\"lastUpdated\":\"2022-03-11T11:29:40.029718Z\",\"expired\":\"2023-11-27T10:15:00Z\",\"chunks\":{\"2\":{\"2023-11-29T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.1\",\"hash\":\"0132815288af51fcfc709f422aee353687654513e4b105fdc8f6166e2df90bbb\"}},\"3\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"24352fcfac12fa8077f074fe6ba1814d09383db29899a1120ca6e587a0d7399d\"}},\"5\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"9e7556604c1774b2e5db4721a7a8ca4fe8c1aade97ad6386a0144218cc3bba56\"}},\"6\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"482dd47c5f344538302328c57e7dd211b27274a264d830f98a0d02d5bb163fc3\"}},\"b\":{\"2023-11-29T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"2.1\",\"hash\":\"0132815288af51fcfc709f422aee353687654513e4b105fdc8f6166e2df90bbc\"}},\"f\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"75572ebd77dc4b07f49479b69aa7400c4a73472fd7b516dc1a10aa51dad86ea3\"}},\"8\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"1e84743ec149ed4c5f02abe27dab0ef0bade3e8882a8d2907419b252c7a768b0\"}}}}]
+    """
+
+let mockRevocationPartitionsDeleteResponse = """
     [{\"kid\":\"GSXuNoyWGYo=\",\"id\":null,\"x\":null,\"y\":null,\"lastUpdated\":\"2022-03-09T11:29:40.029718Z\",\"expired\":\"2023-11-27T10:15:00Z\",\"chunks\":{\"2\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"0132815288af51fcfc709f422aee353687654513e4b105fdc8f6166e2df90bbb\"}},\"3\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"24352fcfac12fa8077f074fe6ba1814d09383db29899a1120ca6e587a0d7399d\"}},\"5\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"9e7556604c1774b2e5db4721a7a8ca4fe8c1aade97ad6386a0144218cc3bba56\"}},\"6\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"482dd47c5f344538302328c57e7dd211b27274a264d830f98a0d02d5bb163fc3\"}},\"f\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"75572ebd77dc4b07f49479b69aa7400c4a73472fd7b516dc1a10aa51dad86ea3\"}},\"8\":{\"2023-11-27T10:15:00Z\":{\"type\":\"BLOOMFILTER\",\"version\":\"1.0\",\"hash\":\"1e84743ec149ed4c5f02abe27dab0ef0bade3e8882a8d2907419b252c7a768b0\"}}}}]
     """
 
 let testKid = "GSXuNoyWGYo"
 let testHashId = "0132815288af51fcfc709f422aee353687654513e4b105fdc8f6166e2df90bbb"
+let updatedTestHashId = "0132815288af51fcfc709f422aee353687654513e4b105fdc8f6166e2df90bbc"
 
 class MockRevocationService: RevocationServiceProtocol {
+    enum TestMode {
+        case new
+        case update
+        case delete
+    }
+    
+    var testMode: TestMode = .new
+    
     func getRevocationLists(completion: @escaping RevocationListCompletion) {
-        guard let data = mockRevocationListsResponse.data(using: .utf8) else {
+        let responseString: String
+        switch testMode {
+        case .new:
+            responseString = mockRevocationListsResponse
+        case .update:
+            responseString = mockRevocationListsUpdateResponse
+        case .delete:
+            responseString = mockRevocationListsDeleteResponse
+        }
+        
+        guard let data = responseString.data(using: .utf8) else {
             completion(nil, nil, .nodata)
             return
         }
@@ -57,7 +92,17 @@ class MockRevocationService: RevocationServiceProtocol {
     }
     
     func getRevocationPartitions(for kid: String, completion: @escaping PartitionListCompletion) {
-        guard let data = mockRevocationPartitionsResponse.data(using: .utf8) else {
+        let responseString: String
+        switch testMode {
+        case .new:
+            responseString = mockRevocationPartitionsResponse
+        case .update:
+            responseString = mockRevocationPartitionsUpdateResponse
+        case .delete:
+            responseString = mockRevocationPartitionsDeleteResponse
+        }
+        
+        guard let data = responseString.data(using: .utf8) else {
             completion(nil, nil, .nodata)
             return
         }
@@ -71,11 +116,21 @@ class MockRevocationService: RevocationServiceProtocol {
     }
     
     func getRevocationPartitions(for kid: String, id: String, completion: @escaping PartitionListCompletion) {
-        
+        print(kid)
     }
     
     func getRevocationPartitionChunks(for kid: String, id: String, cids: [String]?, completion: @escaping ZIPDataTaskCompletion) {
-        guard let gzUrl = Bundle(for: type(of: self)).url(forResource: "mockRevocationPartitionChunks", withExtension: "tar.gz") else {
+        let gzFilename: String
+        switch testMode {
+        case .new:
+            gzFilename = "mockRevocationPartitionChunks"
+        case .update:
+            gzFilename = "mockRevocationPartitionChunksUpdate"
+        case .delete:
+            gzFilename = "mockRevocationPartitionChunksUpdate"
+        }
+        
+        guard let gzUrl = Bundle(for: type(of: self)).url(forResource: gzFilename, withExtension: "tar.gz") else {
             completion(nil, .nodata)
             return
         }
@@ -104,7 +159,8 @@ class MockRevocationService: RevocationServiceProtocol {
 }
 
 class RevocationDownloadTests: XCTestCase {
-    let worker = RevocationWorker(service: MockRevocationService())
+    let service = MockRevocationService()
+    lazy var worker = RevocationWorker(service: service)
 
     override func setUpWithError() throws {
         worker.revocationDataManager.clearAllData()
@@ -114,10 +170,11 @@ class RevocationDownloadTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testDownload() throws {
         
         let expectation = XCTestExpectation(description: "Revocations saved")
 
+        service.testMode = .new
         worker.processReloadRevocations { revocationError in
             let revocations = self.worker.revocationDataManager.currentRevocations()
             
@@ -152,5 +209,52 @@ class RevocationDownloadTests: XCTestCase {
         let waiterResult = XCTWaiter.wait(for: [expectation], timeout: 2.0)
         
         XCTAssertEqual(waiterResult, .completed)
+    }
+    
+    func testUpdate() {
+        let newDownloadExpectation = XCTestExpectation(description: "New revocations downloaded")
+
+        service.testMode = .new
+        worker.processReloadRevocations { revocationError in
+            if revocationError == nil {
+                newDownloadExpectation.fulfill()
+            }
+        }
+        
+        let newDownloadResult = XCTWaiter.wait(for: [newDownloadExpectation], timeout: 2.0)
+//        guard newDownloadResult == .completed else {
+//            return
+//        }
+        
+        let updateExpectation = XCTestExpectation(description: "Revocations updated")
+
+        service.testMode = .update
+        worker.processReloadRevocations { revocationError in
+            if revocationError == nil {
+                updateExpectation.fulfill()
+            }
+        }
+        
+        let updateResult = XCTWaiter.wait(for: [updateExpectation], timeout: 2.0)
+        if updateResult == .completed {
+            let slices = self.worker.revocationDataManager.loadSlices(kid: testKid, x: "null", y: "null", section: "2")
+            
+            XCTAssertNotNil(slices)
+            XCTAssert(slices!.count == 1)
+            
+            let slice = slices!.last!
+            XCTAssertNotNil(slice.hashID)
+            XCTAssertEqual(slice.hashID!, updatedTestHashId)
+            XCTAssertNotNil(slice.hashData)
+            
+            do {
+                let testHashDataUrl = Bundle(for: type(of: self)).url(forResource: "updatedTestHash", withExtension: nil)
+                let testHashData = try Data(contentsOf: testHashDataUrl!)
+                
+                XCTAssertEqual(slice.hashData, testHashData)
+            } catch {
+                print(error)
+            }
+        }
     }
 }
